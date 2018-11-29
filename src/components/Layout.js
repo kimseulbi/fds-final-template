7314637b3275d3e4c858b917a9e538433bcaaead
kimseulbi <ksb5166@gmail.com>
2018-11-29 14:05:32 +0900

    Layout추가

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">쇼핑몰</Link>
+        {this.props.children}
+        <div>저작권은 김슬비에게 있습니다.</div>
+      </div>
+    );
+  }
+}
