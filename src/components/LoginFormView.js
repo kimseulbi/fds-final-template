c5be63ee5137a6e9309acf902c301b3016649c4f
kimseulbi <ksb5166@gmail.com>
2018-11-29 16:45:17 +0900

    로그인 했을때 로그인 목록 표시

--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class LoginFormView extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ export default class LoginFormView extends Component {
     this.state = {
       username: '',
       password: '',
+      success: false, //로그인 성공 여부를 상태값으로 저장
     };
   }
 
@@ -22,14 +24,23 @@ export default class LoginFormView extends Component {
     });
   }
 
-  handleLoginButtonClick() {
+  async handleLoginButtonClick() {
     const { onLogin } = this.props;
     const { username, password } = this.state;
-    onLogin(username, password);
+    await onLogin(username, password);
+    // 로그인이 성공적으로 끝났을 때
+    this.setState({
+      success: true,
+    });
+    // Redirect 컴포넌트를 렌더링 -> 주소표시줄의 상태가 바뀜
   }
 
   render() {
-    const { username, password } = this.state;
+    const { username, password, success } = this.state;
+    // 여기 상태는 나가지면 로그인 페이지로 들어갈수 있지 않나?
+    if (success) {
+      return <Redirect to="/" />;
+    }
     return (
       <div>
         <input
@@ -42,7 +53,7 @@ export default class LoginFormView extends Component {
           value={password}
           onChange={e => this.handlePasswordChange(e)}
         />
-        <button onClick={() => this.handleLoginButton()}>로그인</button>
+        <button onClick={() => this.handleLoginButtonClick()}>로그인</button>
       </div>
     );
   }
--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -3,13 +3,6 @@ import LoginFormView from '../components/LoginFormView';
 import { withUser } from '../contexts/UserContext';
 
 class LoginForm extends Component {
-  static defaultProps = {
-    login: (username, password) => {},
-  };
-  constructor(props) {
-    super(props);
-    const username = e.target.elements.username.value;
-  }
   render() {
     const { login } = this.props;
     return <LoginFormView onLogi={login} />;
