8683009892ff3969548255033478ebe22a802c0c
kimseulbi <ksb5166@gmail.com>
2018-11-29 15:02:10 +0900

    수량과 옵션 수정 시 가격이 출력되는 기능

--- src/components/ProductDetailView.js
+++ src/components/ProductDetailView.js
@@ -19,6 +19,32 @@ class ProductDetailView extends Component {
   };
   // 이페이지에서만 사용하는 상태라면 view에 두어도 된다.
   // UI 상태를 변경은 view에 두어도 된다.
+  constructor(props) {
+    super(props);
+    // 선택된 id를 저장함
+    // 장바구니 저장들 위해서 옵션id가 필요하기 때문에
+    this.state = {
+      quantity: 1,
+      selectedOptionId: null,
+    };
+  }
+  async componentDidMount() {
+    this.setState({
+      quantity: 1,
+      selectedOptionId: '',
+    });
+  }
+
+  handlsOptionChange(e) {
+    this.setState({
+      selectedOptionId: parseInt(e.target.value),
+    });
+  }
+  handleQuantiyChange(e) {
+    this.setState({
+      quantity: parseInt(e.target.value),
+    });
+  }
   render() {
     const {
       id,
@@ -28,13 +54,26 @@ class ProductDetailView extends Component {
       detailImgUrls,
       options,
     } = this.props;
+    const { quantity, selectedOptionId } = this.state;
+    // 첫페이지에서는 undfind
+    const selectedOption = options.find(o => o.id === selectedOptionId);
+    const totalPrice = selectedOption && selectedOption.price * quantity;
     return (
       <div>
-        <select>
+        <select
+          value={selectedOptionId}
+          onChange={e => this.handlsOptionChange(e)}
+        >
           {options.map(o => (
             <option value={o.id}>{o.title}</option>
           ))}
         </select>
+        <input
+          type="number"
+          value={quantity}
+          onChange={e => this.handleQuantiyChange(e)}
+        />
+        <div>가격: {totalPrice}</div>
         <div>{id}</div>
         <div>{title}</div>
         <div>{description}</div>
