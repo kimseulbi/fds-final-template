5df6bbbabb44f0e08c12bf384e65501bf8cb504b
kimseulbi <ksb5166@gmail.com>
2018-11-29 12:46:14 +0900

    주소 표시줄과 상품 정보 화면 연동

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -3,6 +3,10 @@ import ProductDetailView from '../components/ProductDetailView';
 import api from '../api';
 
 export default class ProductDetail extends Component {
+  static defaultPros = {
+    // 표시해주어야 하는 상품의 id
+    productId: null,
+  };
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +19,8 @@ export default class ProductDetail extends Component {
     };
   }
   async componentDidMount() {
-    const { data: product } = await api.get('/products/1');
+      const { productId } = this.props;
+      const { data: product } = await api.get('/products/' + productId);
     this.setState({
       ...product,
       loading: false,
@@ -24,7 +29,7 @@ export default class ProductDetail extends Component {
   render() {
     const product = {
       id: 1,
-      title: '자켓',
+      title: '자켓', 
       description: '따듯해요',
       mainImgUrl: '',
       detailImgUrls: [''],
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ProductDetail from '../containers/ProductDetail';
 
+// prop도 받고 상태도 가지고 있음
 export default class ProductPage extends Component {
   render() {
+    const { match } = this.props;
+    const productId = match.params.productId;
     return (
       <div>
-        <ProductDetail />
+        <ProductDetail productId={productId} />
       </div>
     );
   }
