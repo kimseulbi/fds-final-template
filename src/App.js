1612665ef8d3128f397d07db334bf81833d772cc
kimseulbi <ksb5166@gmail.com>
2018-11-29 15:47:52 +0900

    loginform 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import LoginPage from './pages/LoginPage';
 
+// 주소연결 !!! 라우터
 export default class App extends Component {
   render() {
     return (
@@ -13,6 +15,7 @@ export default class App extends Component {
           {/* exact prop이 없을 때 = 주소가 path로 시작하면 페이지가 그려짐 */}
           <Route exact path="/" component={HomePage} />
           <Route path="/product/:productId" component={ProductPage} />
+          <Route path="/login" component={LoginPage} />
         </>
       </BrowserRouter>
     );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,7 @@ export default class Layout extends Component {
     return (
       <div>
         <Link to="/">쇼핑몰</Link>
+        <Link to="/login">로그인</Link>
         {this.props.children}
         <div>저작권은 김슬비에게 있습니다.</div>
       </div>
