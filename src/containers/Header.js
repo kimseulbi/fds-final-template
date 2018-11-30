8dd9f4c0524a5d356bb4f0e6ddf73860c1f5c02b
kimseulbi <ksb5166@gmail.com>
2018-11-30 11:24:23 +0900

    코드 정리

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { withUser, UserConsumer } from '../contexts/UserContext';
 import HearderView from '../components/HearderView';
-// 헤더 뷰를 그려주면서 상태값을 
-export default function Header(props){
-    return <UserConsumer>
-        {value => <HearderView key={value.username} {...value}/>}
+// 헤더 뷰를 그려주면서 상태값을
+export default function Header(props) {
+  return (
+    <UserConsumer>
+      {value => <HearderView key={value.username} {...value} />}
     </UserConsumer>
+  );
 }
 // 의존성이 생기게 만는 놈이 기떄문에 실무에서 이렇게 만들 만듦.
 // 이렇게 사용 하면 스트리북 작성시 좋습니다.
